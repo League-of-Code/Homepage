e2d6953003656e7561f0497bc2fb442742583a7c
ErrorRPG <J.Radion@yahoo.de>
2020-10-04 20:54:23 +0200

    4. Änderung von Radion
    - Animation für Karte

---
+++ javascript/karte.js
@@ -0,0 +1,12 @@
+var karte = document.getElementById("deutschland");
+
+function klick(){
+    bewegeElement();
+}
+
+function bewegeElement(){
+    karte.style.transform = "translate(-34vw, 0)";
+}
+//karte.style.position = "absolute";
+//karte.style.left = pos_x + "px";
+//karte.style.top = pos_y + "vh";
