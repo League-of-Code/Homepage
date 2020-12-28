5d03591276221c54af1474df61185a5069ddc7ed
Cem <66869029+cemGr@users.noreply.github.com>
2020-12-28 19:34:03 +0100

    Update ping.php

--- corona_projekt/php/testfunctions/ping.php
+++ corona_projekt/php/testfunctions/ping.php
@@ -1,13 +1,15 @@
 <?php
 
 
-    $host = 'https://www.leagueofcode.de'; 
+    $host = 'https://www.leagueofcode.de';
     $port = 80;
     $waitTimeoutInSeconds = 1;
     if($fp = fsockopen($host,$port,$errCode,$errStr,$waitTimeoutInSeconds)){
      // It worked
+     echo "it works";
     } else {
-     // It didn't work
+     // It didn't works
+     echo "It doesnt works";
     }
     fclose($fp);
 
