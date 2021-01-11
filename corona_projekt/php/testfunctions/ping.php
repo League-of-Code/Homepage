70d5e58d0aeb4b3d45b99467edabd951b3c565e9
Cem <66869029+cemGr@users.noreply.github.com>
2021-01-11 11:27:15 +0100

    ping update

--- corona_projekt/php/testfunctions/ping.php
+++ corona_projekt/php/testfunctions/ping.php
@@ -1,24 +1,15 @@
 <?php
 
 
-function urlExists($url=NULL)
-{
-    if($url == NULL) return false;
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $data = curl_exec($ch);
-    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    if($httpcode>=200 && $httpcode<300){
-        return true;
-    } else {
-        return false;
-    }
+function url_exists($url) {
+    return curl_init($url) !== false;
 }
-
-echo urlExists('https://www.leagueofcode.de:80');
+if(url_exists("https://stackoverflow.com/questions/2280394/how-can-i-check-if-a-url-exists-via-php")){
+  echo "its avaiable, oh yea";
+}
+else {
+  echo "it doesnt exist, boy";
+}
 
 
  ?>
