e0e7c9ba932689fae85a4e2999c1f76ecde17690
Cem <66869029+cemGr@users.noreply.github.com>
2021-01-18 16:59:56 +0100

    upload

---
+++ corona_projekt/firebase/firebasetest.js
@@ -0,0 +1,30 @@
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBPfa2kfTNBsTLQM66e_Zq7goujyZJLs6Y",
+    authDomain: "coronavisual-fe087.firebaseapp.com",
+    projectId: "coronavisual-fe087",
+    storageBucket: "coronavisual-fe087.appspot.com",
+    messagingSenderId: "686560350516",
+    appId: "1:686560350516:web:1eb92bd473adf677109561",
+    measurementId: "G-6GWS50GTKZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+
+  var firestore = firebase.firestore();
+
+  var output = firestore.collection("storage").get().then((querySnapshot) => {
+    querySnapshot.foreach((doc) => {
+      console.log('${doc.id} => ${doc.data()}');
+    });
+  });
+
+  const subbutton = document.querySelector("#button1");
+
+  subbutton.addEventListener("click", function(){
+    console.log("hallo"+ output );
+  })
