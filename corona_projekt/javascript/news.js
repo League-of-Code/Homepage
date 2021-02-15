640e4a43da1cfc24a96bfe8184864b575733e5a0
Cem <66869029+cemGr@users.noreply.github.com>
2021-02-15 17:02:39 +0100

    Update news.js

--- corona_projekt/javascript/news.js
+++ corona_projekt/javascript/news.js
@@ -7,9 +7,9 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     newstickerbox = this.responseText;
-    if(visualize){
+
       console.log("Hello world!");
-    }
+    
   }
 };
 
